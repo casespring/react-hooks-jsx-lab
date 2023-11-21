e383298ba8d12f398d7a59d9fbaf473fcbbfe632
Marco Upia <markremembers@gmail.com>
2023-11-20 21:54:47 -0500

    solved

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>"About me"</h2>
+    <p>I will be the greatest that ever was!</p>
+    <img src={image} alt="I made this" />
+  </div>);
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const brickStyle = {
+    color: "firebrick"
+  }
+
+  return (
+  <div id="home">
+    <h1 style={brickStyle}>{name} is a Web Developer from {city}</h1>
+  </div>);
 }
 
 export default Home;
